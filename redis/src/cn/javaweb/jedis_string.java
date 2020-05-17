e40d2fccf90ebb34dd58cda2cd4471aaba6e19de
sumin <sumin08@163.com>
2020-05-18 01:14:39 +0800

    javaweb
    新增jedis操作string

---
+++ redis/src/cn/javaweb/jedis_string.java
@@ -0,0 +1,23 @@
+package cn.javaweb;
+
+import redis.clients.jedis.Jedis;
+
+public class jedis_string {
+    public static void main(String[] args) {
+        //获取连接
+        Jedis jedis = new Jedis("localhost", 6379); //如果使用空参构造,默认值"localhost", 6379
+
+        //存储操作
+        jedis.set("username", "zhansan");
+
+        //获取
+        String username = jedis.get("username");
+        System.out.println("username: "+username);
+
+        //可以使用setex()方法存储可以指定过期时间的key value
+        jedis.setex("username",5,"hehe");//将activecode：hehe键值对存入redis，并且20秒后自动删除该键值对
+
+        //关闭连接
+        jedis.close();
+    }
+}
